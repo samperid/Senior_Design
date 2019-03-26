68eb148deb589ddbdabd41408656c5a1a7d5baae
samperid <samperid@bu.edu>
2019-03-25 20:56:09 -0400

    ttl python script

---
+++ Image_Script/ttl_test.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(0, GPIO.OUT)
+
+GPIO.output(0,True) 
+time.sleep(5)
+GPIO.output(0,False)
+GPIO.output(0,True) 
+time.sleep(5)
+GPIO.output(0,False)
+GPIO.output(0,True) 
+time.sleep(5)
+GPIO.output(0,False)
