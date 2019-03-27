50e6f5b4d0c13f9ebb59fd31fc2cfd7b8a439d2c
samperid <samperid@bu.edu>
2019-03-27 15:27:49 -0400

    update

--- Image_Script/ttl_test.py
+++ Image_Script/ttl_test.py
@@ -4,6 +4,11 @@ import time
 print("testing script")
 GPIO.setmode(GPIO.BCM)
 
+ttl_pin = 11
+
+GPIO.setup(ttl_pin, GPIO.OUT)
+
+'''
 GPIO.setup(11, GPIO.OUT)
 
 GPIO.output(11,True) 
@@ -17,3 +22,4 @@ time.sleep(5)
 GPIO.output(11,True) 
 time.sleep(5)
 GPIO.output(11,False)
+'''
