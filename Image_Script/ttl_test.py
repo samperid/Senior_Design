cdc7f87c5422257c5d0fc9c7b7b572ea298c243d
samperid <samperid@bu.edu>
2019-03-27 14:58:38 -0400

    changed back to RPi.GPIO

--- Image_Script/ttl_test.py
+++ Image_Script/ttl_test.py
@@ -1,21 +1,18 @@
-from periphery import GPIO
+import RPi.GPIO as GPIO
 import time
 
-GPIO_out = GPIO(0, "out")
+GPIO.setmode(GPIO.BCM)
 
+GPIO.setup(0, GPIO.OUT)
 
-GPIO_out.write(True) 
+GPIO.output(0,True) 
 time.sleep(5)
-GPIO_out.write(False)
-time.sleep(5) 
-GPIO_out.write(True) 
+GPIO.output(0,False)
 time.sleep(5)
-GPIO_out.write(False)
-time.sleep(5) 
-GPIO_out.write(True) 
+GPIO.output(0,True) 
 time.sleep(5)
-GPIO_out.write(False)
-time.sleep(5) 
-
-GPIO_out.close()
-
+GPIO.output(0,False)
+time.sleep(5)
+GPIO.output(0,True) 
+time.sleep(5)
+GPIO.output(0,False)
