8f8abb0b0ba7e6c4766dd5d412ab657e7f457d72
samperid <samperid@bu.edu>
2019-03-27 15:17:22 -0400

    update

--- Image_Script/ttl_test.py
+++ Image_Script/ttl_test.py
@@ -1,6 +1,10 @@
 import RPi.GPIO as GPIO
 import time
 
+print("testing script")
+mode = GPIO.getmode()
+print(mode)
+'''
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(3, GPIO.OUT)
@@ -16,3 +20,4 @@ time.sleep(5)
 GPIO.output(0,True) 
 time.sleep(5)
 GPIO.output(0,False)
+'''
