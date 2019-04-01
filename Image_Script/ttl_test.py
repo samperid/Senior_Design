69f4f2af2fb77dc8445d37feceaa7b99584b5c0f
samperid <samperid@bu.edu>
2019-04-01 15:23:03 -0400

    updated python code

--- Image_Script/ttl_test.py
+++ Image_Script/ttl_test.py
@@ -1,8 +1,22 @@
 import RPi.GPIO as GPIO
 import time
 
+GPIO.setmode(GPIO.BCM)
+
 GPIO.setup(14,GPIO.OUT)
 
+GPIO.output(14, GPIO.LOW)
+time.sleep(3)
+GPIO.output(14, GPIO.HIGH)
+time.sleep(3)
+GPIO.output(14, GPIO.LOW)
+time.sleep(3)
+GPIO.output(14, GPIO.HIGH)
+time.sleep(3)
+GPIO.output(14, GPIO.LOW)
+time.sleep(3)
+GPIO.output(14, GPIO.HIGH)
+time.sleep(3)
 '''
 GPIO.setup(11, GPIO.OUT)
 
