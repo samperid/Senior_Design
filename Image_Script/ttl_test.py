51adf599d4c646f99dec090d9f53e63d608cc3c7
samperid <samperid@bu.edu>
2019-04-16 18:51:37 -0400

    update

--- Image_Script/ttl_test.py
+++ Image_Script/ttl_test.py
@@ -5,9 +5,9 @@ GPIO.setmode(GPIO.BCM)
 
 #GPIO.setup(18,GPIO.OUT)
 GPIO.setup(27,GPIO.OUT)
-#GPIO.output(24, GPIO.HIGH)
-#time.sleep(3)
-
+GPIO.output(27, GPIO.HIGH)
+time.sleep(3)
+GPIO.output(27, GPIO.LOW)
 '''
 GPIO.output(18, GPIO.LOW)
 GPIO.output(23, GPIO.HIGH)
