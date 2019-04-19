c649791647a2c096cda207ae9c599ca6ea49a051
samperid <samperid@bu.edu>
2019-04-18 22:02:40 -0400

    updated trigger script

--- Image_Script/trigger.py
+++ Image_Script/trigger.py
@@ -10,9 +10,16 @@ tlf = pylon.TlFactory.GetInstance()
 cam = pylon.InstantCamera(tlf.CreateFirstDevice())
 cam.Open()
 
-cam.TriggerSelector.SetValue("FrameBurstStart")
+cam.TriggerSelector.SetValue("FrameStart")
 cam.TriggerActivation.SetValue("RisingEdge")
 cam.TriggerSource.SetValue("Line1")
 cam.TriggerMode.SetValue("On")
+cam.AcquisitionStatusSelector.SetValue("FrameStart")
+
+bool isWaitingForFrameStart = cam.AcquisitionStatus.GetValue()
+
+if(isWaitingForFrameStart){
+	#it is now safe to apply frame start trigger signals
+}
 
 #cam.TriggerDelayAbs.SetValue(300)
