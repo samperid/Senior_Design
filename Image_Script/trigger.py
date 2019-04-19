0271497a303c51651d68031a85610887338e2f25
samperid <samperid>
2019-04-19 21:08:31 +0000

    adjusted test script to include triggering

--- Image_Script/trigger.py
+++ Image_Script/trigger.py
@@ -1,25 +1,32 @@
 import RPi.GPIO as GPIO
 from pypylon import pylon
+import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(27,GPIO.OUT)
-#GPIO.output(27, GPIO.HIGH)
 
 tlf = pylon.TlFactory.GetInstance()
 cam = pylon.InstantCamera(tlf.CreateFirstDevice())
 cam.Open()
 
-cam.TriggerSelector.SetValue("FrameStart")
-cam.TriggerActivation.SetValue("RisingEdge")
+#cam.TriggerActivation.SetValue("FrameBurstStart")
 cam.TriggerSource.SetValue("Line1")
 cam.TriggerMode.SetValue("On")
-cam.AcquisitionStatusSelector.SetValue("FrameStart")
+#cam.TriggerActivation.SetValue("RisingEdge")
 
-bool isWaitingForFrameStart = cam.AcquisitionStatus.GetValue()
 
-if(isWaitingForFrameStart){
-	#it is now safe to apply frame start trigger signals
-}
 
+#cam.AcquisitionStatusSelector.SetValue("FrameStart")
+#isWaitingForFrameStart = cam.AcquisitionStatus.GetValue()
+
+'''
+for i in range(0,4):
+	if i % 2 == 0:
+		GPIO.output(27, GPIO.HIGH)
+		if(isWaitingForFrameStart):
+			#it is now safe to apply frame start trigger signals
+			cam.TriggerSelector.SetValue("FrameStart")
+	time.sleep(1)
 #cam.TriggerDelayAbs.SetValue(300)
+'''
