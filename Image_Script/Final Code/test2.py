b6abbc5ed4b17308a8df4eaa9b89bc624be0c1a4
samperid <samperid@bu.edu>
2019-05-02 14:23:54 -0400

    added for led testing

--- Image_Script/Final Code/test2.py
+++ Image_Script/Final Code/test2.py
@@ -1,19 +1,97 @@
-import board
-import digitalio
-import busio
- 
-print("Hello blinka!")
- 
-# Try to great a Digital input
-pin = digitalio.DigitalInOut(board.D4)
-print("Digital IO ok!")
- 
-# Try to create an I2C device
-i2c = busio.I2C(board.SCL, board.SDA)
-print("I2C ok!")
- 
-# Try to create an SPI device
-spi = busio.SPI(board.SCLK, board.MOSI, board.MISO)
-print("SPI ok!")
- 
-print("done!")
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+#410nm
+GPIO.setup(4,GPIO.OUT)
+#490nm
+GPIO.setup(27,GPIO.OUT)
+#565nm
+GPIO.setup(22,GPIO.OUT)
+#625nm
+GPIO.setup(24,GPIO.OUT)
+#730nm
+GPIO.setup(25,GPIO.OUT)
+#808nm
+GPIO.setup(5,GPIO.OUT)
+#940
+GPIO.setup(6,GPIO.OUT)
+
+GPIO.output(4,GPIO.LOW)
+GPIO.output(27,GPIO.LOW)
+GPIO.output(22,GPIO.LOW)
+GPIO.output(24,GPIO.LOW)
+GPIO.output(25,GPIO.LOW)
+GPIO.output(5,GPIO.LOW)
+GPIO.output(6,GPIO.LOW)
+# time.sleep(0.5)
+time.sleep(1)
+
+#Acquire each individual image per wavelength 
+print("Turn on 410nm LED")
+GPIO.output(4,GPIO.HIGH)
+# time.sleep(0.5)
+time.sleep(1)
+
+print("Acquiring 410nm Image")
+file_name = name+"_410"
+GPIO.output(4,GPIO.LOW)
+# time.sleep(0.5)
+time.sleep(1)
+
+print("Turn on 490nm LED")
+GPIO.output(27,GPIO.HIGH)
+# time.sleep(0.5)
+time.sleep(1)
+
+print("Acquiring 490nm Image")
+file_name = name+"_490"
+GPIO.output(27,GPIO.LOW)
+# time.sleep(0.5)
+time.sleep(1)
+
+# print("Turn on 565nm LED")
+# GPIO.output(22,GPIO.HIGH)
+# # time.sleep(0.5)
+# time.sleep(1)
+
+# print("Acquiring 565nm Image")
+# file_name = name+"_565"
+# acquire_img(file_name)
+# GPIO.output(22,GPIO.LOW)
+# # time.sleep(0.5)
+# time.sleep(0.5)
+
+print("Turn on 625nm LED")
+GPIO.output(24,GPIO.HIGH)
+# time.sleep(0.5)
+time.sleep(1)
+
+print("Acquiring 625nm Image")
+file_name = name+"_625"
+GPIO.output(24,GPIO.LOW)
+# time.sleep(0.5)
+time.sleep(1)
+
+print("Turn on 730nm LED")
+GPIO.output(25,GPIO.HIGH)
+# time.sleep(0.5)
+time.sleep(1)
+
+print("Acquiring 730nm Image")
+file_name = name+"_730"
+GPIO.output(25,GPIO.LOW)
+# time.sleep(0.5)
+time.sleep(1)
+
+print("Turn on 808nm LED")
+GPIO.output(5,GPIO.HIGH)
+# time.sleep(0.5)
+time.sleep(1)
+
+print("Acquiring 808nm Image")
+file_name = name+"_808"
+GPIO.output(5,GPIO.LOW)
+# time.sleep(0.5)
+time.sleep(1)
